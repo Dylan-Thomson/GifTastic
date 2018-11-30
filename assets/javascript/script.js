933075ba4a718797b2599d3b933982f07c292632
Dylan Thomson <djthomson88@gmail.com>
2018-11-30 15:33:01 -0500

    Some refactoring, start working on 'get ten more' functionality

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,34 +5,61 @@ function getGIFs(searchTerm) {
         method: "GET"
     }).then(function(response) {
         console.log(response);
-        displayGIFs(response);
+        displayGIFs(response, searchTerm);
     });
 }
 
-function displayGIFs(gifs) {
+function displayGIFs(gifs, searchTerm) {
+    console.log(gifs);
+    $("#gif-container").empty();
     gifs.data.forEach(function(gif) {
-        var img = $("<img>");
-        img.attr("data-still", gif.images.fixed_height_small_still.url);
-        img.attr("data-animate", gif.images.fixed_height_small.url);
-        img.attr("data-state", "still");
-        img.attr("src", img.attr("data-still"));
-        img.on("click", () => {
-            if(img.attr("data-state") === "still") {
-                img.attr("src", img.attr("data-animate"));
-                img.attr("data-state", "animate");
-            }
-            else {
-                img.attr("src", img.attr("data-still"));
-                img.attr("data-state", "still");
-            }
-        });
-        var div = $("<div>").append(img);
-        var p = $("<p>");
-        p.text("Rating: " + gif.rating);
-        div.append(p);
-        div.addClass("d-inline-block m-3");
-        $("#gif-container").append(div);
+        displayGIF(gif);
     });
+    // var button = $("<button>");
+    // button.addClass("btn btn-primary m-3");
+    // button.attr("data-offset", 10);
+    // button.text("More...");
+    // button.on("click", () => {
+    //     // console.log(Number(button.attr("data-offset")) + 10);
+    //     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=dvcHv6i1zrERIdmKZ2fROgBXIsZTvhAE&limit=10&offset=" + button.attr("data-offset");
+    //     $.ajax({
+    //         url: queryURL,
+    //         method: "GET"
+    //     }).then(function(response) {
+    //         console.log(response);
+    //         displayGIFs(response, searchTerm);
+    //     });
+
+    //     var newOffset = Number(button.attr("data-offset")) + 10;
+    //     button.attr("data-offset", newOffset);
+    //     console.log(newOffset);
+    // });
+    // $("#gif-container").append(button);
+}
+
+function displayGIF(gif) {
+    var img = $("<img>");
+    img.attr("data-still", gif.images.fixed_height_small_still.url);
+    img.attr("data-animate", gif.images.fixed_height_small.url);
+    img.attr("data-state", "still");
+    img.attr("src", img.attr("data-still"));
+    img.on("click", () => {
+        if(img.attr("data-state") === "still") {
+            img.attr("src", img.attr("data-animate"));
+            img.attr("data-state", "animate");
+        }
+        else {
+            img.attr("src", img.attr("data-still"));
+            img.attr("data-state", "still");
+        }
+    });
+    var div = $("<div>").append(img);
+    var p = $("<p>");
+    p.text("Rating: " + gif.rating);
+    p.addClass("text-center");
+    div.append(p);
+    div.addClass("d-inline-block m-3");
+    $("#gif-container").append(div);
 }
 
 function renderButtons(categories) {
