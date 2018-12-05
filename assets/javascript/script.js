b70662016e5ed460863687d8fff45997cc9b8f8d
Dylan Thomson <djthomson88@gmail.com>
2018-12-05 13:53:55 -0500

    Some refactoring

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,28 +3,23 @@ class GifTastic {
         this.categories = categories;
         this.key = key;
         this.offset = 0;
-        // this.favorites = [];
 
+        // Try to get favorites from local storage, initialize to empty array if none found
         this.favorites = JSON.parse(localStorage.getItem("favorites"));
         if(!Array.isArray(this.favorites)) {
             this.favorites = [];
         }
     }
 
+    // Retrieve 10 GIFS using given search value
     getGIFs(searchTerm) {
         this.searchTerm = searchTerm;
         this.offset = 0;
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=" + this.key + "&limit=10";
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then((response) => {
-            console.log(response);
-            $("#gif-container").empty();
-            this.displayGIFs(response);
-        });
+        this.getData(queryURL);
     }
 
+    // Retrieve the next 10 GIFS of current category
     getMoreGIFs() {
         this.offset += 10;
         var queryURL;
@@ -34,16 +29,11 @@ class GifTastic {
         else {
             queryURL = "https://api.giphy.com/v1/gifs/search?q=" + this.searchTerm + "&api_key=" + this.key + "&limit=10&offset=" + this.offset;
         }
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then((response) => {
-            console.log(response);
-            this.displayGIFs(response);
-        });
+        this.getData(queryURL);
     }
 
     displayGIFs(gifs) {
+        $("#gif-container").empty();
         gifs.data.forEach((gif) => {
             this.displayGIF(gif);
         });
@@ -52,11 +42,6 @@ class GifTastic {
 
     displayGIF(gif) {
         var img = $("<img>");
-        // img.attr("data-still", gif.images.fixed_height_small_still.url);
-        // img.attr("data-animate", gif.images.fixed_height_small.url);
-
-        // img.attr("data-still", gif.images.fixed_width_still.url);
-        // img.attr("data-animate", gif.images.fixed_width.url);
         img.attr("data-still", gif.images.fixed_height_still.url);
         img.attr("data-animate", gif.images.fixed_height.url);
         img.attr("data-state", "still");
@@ -83,11 +68,9 @@ class GifTastic {
         var favoriteIcon = $("<i>");
 
         if(this.favorites.indexOf(gif.id) >= 0) {
-            // console.log(this.favorites, "true");
             favoriteIcon.addClass("fas fa-heart pink");
         }
         else {
-            // console.log(this.favorites, "false");
             favoriteIcon.addClass("far fa-heart");
         }
 
@@ -110,23 +93,12 @@ class GifTastic {
             }
         });
         infoDiv.append(favoriteIcon);
-
-
-
-        // var downloadLink = $("<a>");
-        // downloadLink.attr("href", gif.images.original.url);
-        // downloadLink.attr("download", "");
-        // downloadLink.attr("target", "_blank");
-        // var downloadIcon = $("<i>");
-        // downloadIcon.addClass("fas fa-arrow-down")
-        // downloadLink.append(downloadIcon);
-        // infoDiv.append(downloadLink);
-
         div.append(infoDiv);
         div.addClass("d-inline-block m-3 bg-dark text-white border border-dark");
         $("#gif-container").append(div);
     }
 
+    // Create buttons for each category
     renderButtons() {
         $("#button-container").empty();
         this.categories.forEach((category) => {
@@ -135,17 +107,28 @@ class GifTastic {
             button.text(category);
             button.addClass("btn btn-primary m-2");
             button.on("click", () => {
+                // $("#gif-container").empty();
                 this.getGIFs(category);
             });
             $("#button-container").append(button);
         });
     }
     
+    // Add a new category and render all buttons
     addCategory(category) {
         this.categories.push(category);
         this.renderButtons();
     }
 
+    // Get 10 trending GIFS
+    getTrending() {
+        this.searchTerm = "trending";
+        this.offset = 0;
+        var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=" + this.key + "&limit=10";
+        this.getData(queryURL);
+    }
+        
+    // TODO: Cache favorites api results, not just search value
     getFavorites() {
         this.favorites.forEach((id) => {
             var queryURL = "https://api.giphy.com/v1/gifs/" + id + "?api_key=" + this.key;
@@ -158,38 +141,27 @@ class GifTastic {
             });
         });
     }
-
-    getRandom() {
-        var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=" + this.key;
+    
+    // Get data from API given query and display
+    getData(url) {
         $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then((response) => {
-            console.log(response);
-            this.displayGIF(response.data);
-        });
-    }
-
-    getTrending() {
-        this.searchTerm = "trending";
-        this.offset = 0;
-        var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=" + this.key + "&limit=10";
-        $.ajax({
-            url: queryURL,
+            url: url,
             method: "GET"
         }).then((response) => {
             console.log(response);
             this.displayGIFs(response);
-        });
+        });    
     }
 }
 
-// TODO: Cache favorites api results, not just search value
 
 $(document).ready(function() {
+    // Initialize gitTastic and render buttons
     var categories = ["cats", "dogs", "cows", "pigs"];
     var gifTastic = new GifTastic(categories, "dvcHv6i1zrERIdmKZ2fROgBXIsZTvhAE");
+    gifTastic.renderButtons();
 
+    // Add a new category from input field if not empty
     $("#submit-category").on("click", (event) => {
         event.preventDefault();
         if($("#input-category").val() !== "") {
@@ -198,6 +170,7 @@ $(document).ready(function() {
         }
     });
 
+    // Clear GIF container
     $("#clear-gifs").on("click", (event) => {
         event.preventDefault();
         $("#gif-container").empty();
@@ -205,30 +178,24 @@ $(document).ready(function() {
         gifTastic.offset = 0;
     });
 
+    // Get 10 more of current category
     $("#get-more").on("click", (event) => {
         event.preventDefault();
         gifTastic.getMoreGIFs();
     });
 
+    // Get favorite GIFS
     $("#get-favorites").on("click", (event) => {
         event.preventDefault();
-        $("#gif-container").empty();
         $("#control-button-container").addClass("d-none");
         gifTastic.getFavorites();
     });
-    
-    $("#get-random").on("click", (event) => {
-        event.preventDefault();
-        $("#gif-container").empty();
-        gifTastic.getRandom();
-    });
 
+    // Get trending GIFS
     $("#get-trending").on("click", (event) => {
         event.preventDefault();
-        $("#gif-container").empty();
         gifTastic.getTrending();
     });
 
-    gifTastic.renderButtons();
 
 });
